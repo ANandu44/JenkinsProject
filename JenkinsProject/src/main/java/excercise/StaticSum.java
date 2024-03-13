11ef3c349ca75e8fbda499c7768e18fab2f674e1
Azraq97 <84129096+Azraq97@users.noreply.github.com>
2024-03-13 14:10:26 +0530

    my second commit

---
+++ JenkinsProject/src/main/java/excercise/StaticSum.java
@@ -0,0 +1,17 @@
+package excercise;
+
+public class StaticSum {
+	public static void sum()
+	{
+		int a=3;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		StaticSum.sum();
+
+	}
+
+}
---
+++ JenkinsProject/src/main/java/excercise/StaticSum2.java
@@ -0,0 +1,15 @@
+package excercise;
+
+public class StaticSum2 {
+	public static void sum(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		StaticSum2.sum(5, 5);
+
+	}
+
+}
